8535f38d3f6270627bc37212a393fa58bf8cf94d
moro <ricardoromero4444@gmail.com>
2021-11-08 23:25:10 -0300

    ✨ : Added custom _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import styles from '../styles/home.module.scss';
+import Head from 'next/head'
 
 export default function Home() {
 
   return (
-    <h1 className={styles.title}>
-      hello <span> world </span>
-    </h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+
+        hello <span> world </span>
+      </h1>
+    </>
   )
 }
