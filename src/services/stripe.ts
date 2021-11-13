0e210afbd0b61f8ce7da5ba8f0e28834d1f71e36
moro <ricardoromero4444@gmail.com>
2021-11-13 18:21:05 -0300

    📝 : Added comment warning

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,5 +1,7 @@
 import Stripe from 'stripe'
 
+// DO NOT IMPORT THIS IN CLIENT-SIDE ROUTES.
+
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: '2020-08-27',
   appInfo: {
