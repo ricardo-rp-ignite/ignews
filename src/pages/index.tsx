06198837816b76860da3598d18501b625a852c41
moro <ricardoromero4444@gmail.com>
2021-11-12 22:12:37 -0300

    ✨ : Created homePage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 
+import styles from './Home.module.scss'
+
 export default function Home() {
   return (
     <>
@@ -7,8 +9,18 @@ export default function Home() {
         <title>Home | ig.news</title>
       </Head>
 
-      <main>
-        <section></section>
+      <main className={styles.main}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the posts <br />
+            <span>for $9.90 a month</span>
+          </p>
+        </section>
+
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
