d0c1d060e3fb03764707c64ce5bb10bb14cde8f4
moro <ricardoromero4444@gmail.com>
2021-11-17 22:45:33 -0300

    🐛 : Removed unexisting prop from button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home({ product }: HomeProps) {
             <span>for {formattedAmount} a month</span>
           </p>
 
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
