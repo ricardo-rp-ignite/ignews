141315aecdf5b18e969cf7a2b404dfae5ca85c6b
moro <ricardoromero4444@gmail.com>
2021-11-17 22:47:36 -0300

    💡 : Added client-side disclaimer

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from 'stripe'
 
-// DO NOT IMPORT THIS IN CLIENT-SIDE ROUTES.
+// DO NOT IMPORT THIS IN CLIENT-SIDE CODE.
+// THIS MUST ONLY BE USED IN API ROUTES OR SERVER-RENDERING PAGES
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: '2020-08-27',
