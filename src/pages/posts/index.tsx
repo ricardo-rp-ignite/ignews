35aa92445fa34ac1f2701939f9ec6010e68c3399
Moro <ricardoromero4444@gmail.com>
2022-01-18 18:28:25 -0300

    ✨ : Add link from posts page to posts/id page

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,14 @@
 import { GetStaticProps } from 'next'
+import Link from 'next/link'
 import Head from 'next/head'
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
 
 import { getPrismicClient } from '../../services/prismic'
+import { formatPostDate } from '../../utils/formatPostDate'
 
 import styles from './Posts.module.scss'
+
 type Post = {
   uid: string
   title: string
@@ -24,12 +27,14 @@ export default function Posts({ posts }: PostsProps) {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          {posts.map(post => (
-            <a href="#" key={post.uid}>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.summary}</p>
-            </a>
+          {posts.map(({ uid, updatedAt, title, summary }) => (
+            <Link key={uid} passHref href={`/posts/${uid}`}>
+              <a>
+                <time>{updatedAt}</time>
+                <strong>{title}</strong>
+                <p>{summary}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
@@ -50,10 +55,7 @@ export const getStaticProps: GetStaticProps<PostsProps> = async () => {
       uid: post.uid,
       title: RichText.asText(post.data.title),
       summary: post.data.content.find(c => c.type === 'paragraph')?.text ?? '',
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
-        'pt-BR',
-        { day: '2-digit', month: 'long', year: 'numeric' }
-      ),
+      updatedAt: formatPostDate(post.last_publication_date),
     })
   )
 
